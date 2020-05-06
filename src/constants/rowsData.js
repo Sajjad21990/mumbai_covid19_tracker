7da97dd3fd7870651ed2d69664d9c836e8c9361f
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-05-06 20:31:45 +0530

    oops

--- src/constants/rowsData.js
+++ src/constants/rowsData.js
@@ -15,8 +15,8 @@ const rowData = [
   },
   {
     area_name: "Mira-Bhayandar",
-    total_cases: 9,
-    total_active: 0,
+    total_cases: 10,
+    total_active: 1,
     total_recovered: 8,
     total_deaths: 1,
   },
