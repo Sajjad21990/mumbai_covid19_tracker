c91ac01ba812aa7f0012aca358f1b181040b9868
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-05-09 14:49:45 +0530

    oops

--- src/constants/rowsData.js
+++ src/constants/rowsData.js
@@ -8,8 +8,8 @@ const rowData = [
   },
   {
     area_name: "Pune",
-    total_cases: 12,
-    total_active: 11,
+    total_cases: 14,
+    total_active: 13,
     total_recovered: 1,
     total_deaths: 0,
   },
